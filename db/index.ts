d618013f951025001e7b77f7eaf8ebe051985a1e
gustavocadev <gustavocadev@gmail.com>
2023-07-16 15:04:23 -0500

    chore: use DB_URL

--- db/index.ts
+++ db/index.ts
@@ -2,7 +2,7 @@ import { drizzle, BetterSQLite3Database } from 'drizzle-orm/better-sqlite3';
 import Database from 'better-sqlite3';
 import { migrate } from 'drizzle-orm/better-sqlite3/migrator';
 
-const sqlite = new Database('notes.sqlite');
+const sqlite = new Database(process.env.DB_URL!);
 export const db: BetterSQLite3Database = drizzle(sqlite);
 
 // this is important to bring the schema into the database, otherwise the tables won't be created
