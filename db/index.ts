43517b1c396d2bb896f97a9f048ded93e4c56638
gustavocadev <gustavocadev@gmail.com>
2023-08-02 14:36:54 -0500

    chore: add schema to use query builders

--- db/index.ts
+++ db/index.ts
@@ -1,9 +1,13 @@
 import { drizzle, BetterSQLite3Database } from 'drizzle-orm/better-sqlite3';
 import Database from 'better-sqlite3';
 import { migrate } from 'drizzle-orm/better-sqlite3/migrator';
+import * as schema from './schema/index';
 
 const sqlite = new Database(process.env.DB_URL!);
-export const db: BetterSQLite3Database = drizzle(sqlite);
+export const db: BetterSQLite3Database<typeof schema> = drizzle(sqlite, {
+  // to use query builders like findMany()
+  schema,
+});
 
 // this is important to bring the schema into the database, otherwise the tables won't be created
 migrate(db, { migrationsFolder: 'drizzle' });
