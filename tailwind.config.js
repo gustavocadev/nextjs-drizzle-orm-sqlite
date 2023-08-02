1cd38b9b6b9722cafcd5a48a466878c5c1517f4a
gustavocadev <gustavocadev@gmail.com>
2023-08-02 14:35:27 -0500

    chore: add nextui and use esm

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,17 @@
+import { nextui } from '@nextui-org/react';
+
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
   darkMode: ['class'],
   content: [
     './pages/**/*.{ts,tsx}',
     './components/**/*.{ts,tsx}',
     './app/**/*.{ts,tsx}',
     './src/**/*.{ts,tsx}',
+    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {},
-  plugins: [require('daisyui')],
-  daisyui: {
-    themes: ['light'],
-  },
+  darkMode: 'class',
+  plugins: [nextui()],
 };
