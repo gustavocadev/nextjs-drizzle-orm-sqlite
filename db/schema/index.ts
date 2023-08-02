f27cc66b965cab0349f8e540d0ee55ceb18ba053
gustavocadev <gustavocadev@gmail.com>
2023-08-02 14:37:04 -0500

    chore: export types

--- db/schema/index.ts
+++ db/schema/index.ts
@@ -1,7 +1,10 @@
+import { InferModel } from 'drizzle-orm';
 import { sqliteTable, text } from 'drizzle-orm/sqlite-core';
 
-export const notes = sqliteTable('note', {
+export const notes = sqliteTable('notes', {
   id: text('id').primaryKey(),
   title: text('title').notNull(),
   description: text('description').notNull(),
 });
+
+export type Note = InferModel<typeof notes>;
